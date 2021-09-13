11036621febb05519c2a7c6c8f82bc34a70bd1a2
fivestarter <fivestarter@gmail.com>
2021-09-14 00:19:58 +0300

    v2

---
+++ core/src/ru/fivestarter/chir/model/Car.java
@@ -0,0 +1,9 @@
+package ru.fivestarter.chir.model;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class Car extends GameObject {
+    public Car(Texture texture, float x, float y, float with, float height) {
+        super(texture, x, y, with, height);
+    }
+}
---
+++ core/src/ru/fivestarter/chir/model/GameObject.java
@@ -0,0 +1,21 @@
+package ru.fivestarter.chir.model;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+
+public abstract class GameObject {
+    private Rectangle bounds;
+    private Sprite object;
+
+    public GameObject(Texture texture, float x, float y, float with, float height) {
+        bounds = new Rectangle(x, y, with, height);
+        object = new Sprite(texture);
+    }
+
+    public void draw(SpriteBatch batch) {
+        object.setBounds(bounds.getX(), bounds.getY(), bounds.getWidth(), bounds.getHeight());
+        object.draw(batch);
+    }
+}
