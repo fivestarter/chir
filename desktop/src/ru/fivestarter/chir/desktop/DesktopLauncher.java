6f90f082f0a541705b81698be997812cfcb2b497
Юрий Старцев <yury@MacBook-Pro-Urij.local>
2021-09-13 23:08:02 +0300

    Initial commit

---
+++ desktop/src/ru/fivestarter/chir/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package ru.fivestarter.chir.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.fivestarter.chir.ChirGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Чир";
+		config.width = 800;
+		config.height = 480;
+		new LwjglApplication(new ChirGame(), config);
+	}
+}
