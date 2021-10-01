5c20d057479a674cb793bc5e45180c3ea0e8b5f1
fivestarter <fivestarter@gmail.com>
2021-10-02 02:12:56 +0300

    punch animation was fixed

--- core/src/ru/fivestarter/dichlofos/jrpg/model/Guile.java
+++ core/src/ru/fivestarter/dichlofos/jrpg/model/Guile.java
@@ -13,43 +13,55 @@ public class Guile implements Character {
     public static final float FRAME_DURATION = 0.25f;
     private final Animation<TextureRegion> idleAnimation;
     private final Animation<TextureRegion> punchAnimation;
-    private Animation<TextureRegion> currentAnimation;
 
     private final Sprite sprite;
-    private float stateTime = 0f;
-    private float activeStateTime = 0f;
+    private float idleTime = 0f;
+    private float punchDuration = 0f;
+    private float punchTime = 0f;
     private GuileController guileController;
 
     public Guile(TextureAtlas textureAtlas, int x, int y, int with, int height) {
         this.idleAnimation = createWalkAnimation(textureAtlas);
         this.punchAnimation = createPunchAnimation(textureAtlas);
-        this.currentAnimation = idleAnimation;
-        this.sprite = new Sprite(idleAnimation.getKeyFrame(stateTime, true), x, y, with, height);
+        this.sprite = new Sprite(idleAnimation.getKeyFrame(idleTime, true), x, y, with, height);
         this.sprite.setSize(with, height);
         this.sprite.setPosition(x, y);
         this.guileController = new GuileController(this);
     }
 
     public void draw(SpriteBatch batch) {
-        stateTime += Gdx.graphics.getDeltaTime();
-        sprite.setRegion(currentAnimation.getKeyFrame(stateTime));
-        sprite.draw(batch);
         guileController.handle();
 
-        if (isActiveState()) {
-            activeStateTime -= Gdx.graphics.getDeltaTime();
-            if (!isActiveState()) {
-                currentAnimation = idleAnimation;
-            }
+        if (isPunching()) {
+            punchDuration -= Gdx.graphics.getDeltaTime();
+            animatePunch();
+        } else {
+            animateIdle();
         }
+
+        sprite.draw(batch);
     }
 
     @Override
     public void punch() {
-        if (!isActiveState()) {
-            currentAnimation = punchAnimation;
-            activeStateTime = FRAME_DURATION;
+        if (!isPunching()) {
+            punchTime = 0;
+            punchDuration = FRAME_DURATION * 2; // не понятно почему так
         }
+    }
+
+    private boolean isPunching() {
+        return punchDuration > 0;
+    }
+
+    private void animateIdle() {
+        idleTime += Gdx.graphics.getDeltaTime();
+        sprite.setRegion(idleAnimation.getKeyFrame(idleTime));
+    }
+
+    private void animatePunch() {
+        punchTime += Gdx.graphics.getDeltaTime();
+        sprite.setRegion(punchAnimation.getKeyFrame(punchTime));
     }
 
     private Animation<TextureRegion> createWalkAnimation(TextureAtlas textureAtlas) {
@@ -66,9 +78,5 @@ public class Guile implements Character {
                 .flatMap(Stream::of)
                 .toArray(TextureRegion[]::new);
         return new Animation<>(FRAME_DURATION, new Array<>(punchFrames), Animation.PlayMode.LOOP_PINGPONG);
-    }
-
-    private boolean isActiveState() {
-        return activeStateTime > 0;
     }
 }
