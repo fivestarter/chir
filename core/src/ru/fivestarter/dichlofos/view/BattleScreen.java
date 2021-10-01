74305b86d457b6cbf10f119f14fb93e177cb2d2a
fivestarter <fivestarter@gmail.com>
2021-10-01 18:31:21 +0300

    battle scene background was added

--- core/src/ru/fivestarter/dichlofos/view/BattleScreen.java
+++ core/src/ru/fivestarter/dichlofos/view/BattleScreen.java
@@ -3,18 +3,21 @@ package ru.fivestarter.dichlofos.view;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class BattleScreen implements Screen {
 
-    SpriteBatch batch;
-    BitmapFont font;
+    private SpriteBatch batch;
+    private BitmapFont font;
+    private Texture background;
 
     @Override
     public void show() {
         batch = new SpriteBatch();
         font = new BitmapFont();
+        background = new Texture(Gdx.files.internal("scene/pun.png"));
     }
 
     @Override
@@ -22,7 +25,7 @@ public class BattleScreen implements Screen {
         Gdx.gl.glClearColor(.25f, .25f, .25f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        font.draw(batch, "Happy Coding!", (float) Gdx.graphics.getWidth() / 2, (float) Gdx.graphics.getHeight() / 2);
+        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         batch.end();
     }
 
