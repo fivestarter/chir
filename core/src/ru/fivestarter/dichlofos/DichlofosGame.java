cc1522e18e060031d4a0d98cd0752a7714b61c0c
fivestarter <fivestarter@gmail.com>
2023-08-09 00:01:51 +0300

    stable version. may be used

--- core/src/ru/fivestarter/dichlofos/DichlofosGame.java
+++ core/src/ru/fivestarter/dichlofos/DichlofosGame.java
@@ -3,6 +3,7 @@ package ru.fivestarter.dichlofos;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import ru.fivestarter.dichlofos.gta.view.WorldScreen;
 import ru.fivestarter.dichlofos.jrpg.view.BattleScreen;
 import ru.fivestarter.dichlofos.utils.Assets;
 
@@ -14,16 +15,16 @@ public class DichlofosGame extends Game {
     @Override
     public void create() {
         assets = new Assets();
-/*        gameScreen = new WorldScreen(this);
+        gameScreen = new WorldScreen(this);
         ((WorldScreen) gameScreen).setTextureAtlas(getTextureAtlas());
-        setScreen(gameScreen);*/
-        setScreen(new BattleScreen(getTextureAtlas()));
+        setScreen(gameScreen);
+        //setScreen(new BattleScreen(getTextureAtlas()));
     }
 
     @Override
     public void dispose() {
         super.dispose();
-        //gameScreen.dispose();
+        gameScreen.dispose();
         assets.dispose();
     }
 
