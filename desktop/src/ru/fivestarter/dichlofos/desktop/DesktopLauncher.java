e9c10e9c0ad19a5aef8b1db8da625168ff7d9fac
fivestarter <fivestarter@gmail.com>
2023-08-09 14:16:08 +0300

    dependencies were updated (i.e. lwjgl=>lwjgl3)

--- desktop/src/ru/fivestarter/dichlofos/desktop/DesktopLauncher.java
+++ desktop/src/ru/fivestarter/dichlofos/desktop/DesktopLauncher.java
@@ -1,15 +1,14 @@
 package ru.fivestarter.dichlofos.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import ru.fivestarter.dichlofos.DichlofosGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Дихлофос";
-		config.width = 800;
-		config.height = 480;
-		new LwjglApplication(new DichlofosGame(), config);
-	}
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("Дихлофос");
+        config.setWindowedMode(800, 480);
+        new Lwjgl3Application(new DichlofosGame(), config);
+    }
 }
