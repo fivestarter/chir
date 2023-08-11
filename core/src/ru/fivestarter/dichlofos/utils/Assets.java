4981fae48a4856fc91599101e33fd099cc5fc3a4
fivestarter <fivestarter@gmail.com>
2023-08-11 21:59:04 +0300

    hero atlas loading was added

--- core/src/ru/fivestarter/dichlofos/utils/Assets.java
+++ core/src/ru/fivestarter/dichlofos/utils/Assets.java
@@ -5,11 +5,13 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 public class Assets {
     public static final String COMMON_ATLAS_FILE_NAME = "atlas/common/atlas1.atlas";
+    public static final String GTA_HERO_ATLAS_FILE_NAME = "atlas/gta/hero/hero.atlas";
     private final AssetManager manager;
 
     public Assets() {
         this.manager = new AssetManager();
         manager.load(COMMON_ATLAS_FILE_NAME, TextureAtlas.class);
+        manager.load(GTA_HERO_ATLAS_FILE_NAME, TextureAtlas.class);
         manager.finishLoading();
     }
 
@@ -18,6 +20,6 @@ public class Assets {
     }
 
     public void dispose() {
-
+        manager.dispose();
     }
 }
