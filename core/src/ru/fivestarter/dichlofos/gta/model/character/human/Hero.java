b386aa98ec6159ae62dbf074097d29190036ba23
fivestarter <fivestarter@gmail.com>
2023-08-12 04:43:53 +0300

    hero overlapping was improved

--- core/src/ru/fivestarter/dichlofos/gta/model/character/human/Hero.java
+++ core/src/ru/fivestarter/dichlofos/gta/model/character/human/Hero.java
@@ -2,6 +2,7 @@ package ru.fivestarter.dichlofos.gta.model.character.human;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
 import ru.fivestarter.dichlofos.gta.control.HeroController;
 import ru.fivestarter.dichlofos.gta.model.World;
 import ru.fivestarter.dichlofos.gta.model.character.CharacterSprite;
@@ -11,12 +12,16 @@ import static ru.fivestarter.dichlofos.gta.view.WorldScreen.UNIT_SCALE;
 public class Hero extends CharacterSprite {
 
     public static final String SPRITE_NAME = "mh_walk";
-    private static final float WITH = 20f * UNIT_SCALE;
+    private static final float DIMENSION = 20f * UNIT_SCALE;
 
     private final HeroController heroController;
 
     public Hero(TextureRegion textureRegion, float x, float y, World world) {
-        super(textureRegion.split(textureRegion.getRegionWidth() / 3, textureRegion.getRegionHeight() / 4)[1][0], x, y, WITH, WITH);
+        super(textureRegion.split(textureRegion.getRegionWidth() / 3, textureRegion.getRegionHeight() / 4)[1][0],
+                x,
+                y,
+                DIMENSION,
+                DIMENSION);
         this.heroController = new HeroController(this, world);
     }
 
@@ -24,5 +29,11 @@ public class Hero extends CharacterSprite {
     public void draw(Batch batch) {
         super.draw(batch);
         heroController.handle();
+    }
+
+    public Rectangle getFootRectangle() {
+        Rectangle rectangle = getBoundingRectangle();
+        rectangle.setHeight(DIMENSION / 4);
+        return rectangle;
     }
 }
