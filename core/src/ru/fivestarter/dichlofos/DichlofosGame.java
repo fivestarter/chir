713175b9636227b4d33ed3081a651df3a3631dd9
fivestarter <fivestarter@gmail.com>
2024-09-11 01:56:38 +0300

    рефакторинг.

--- core/src/ru/fivestarter/dichlofos/DichlofosGame.java
+++ core/src/ru/fivestarter/dichlofos/DichlofosGame.java
@@ -2,14 +2,10 @@ package ru.fivestarter.dichlofos;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import ru.fivestarter.dichlofos.game.fight.FightScreen;
 import ru.fivestarter.dichlofos.game.gta.view.WorldScreen;
 import ru.fivestarter.dichlofos.menu.view.MenuScreen;
 import ru.fivestarter.dichlofos.utils.Assets;
-
-import static ru.fivestarter.dichlofos.utils.Assets.FIGHT_ATLAS_FILE_NAME;
-import static ru.fivestarter.dichlofos.utils.Assets.GTA_HERO_ATLAS_FILE_NAME;
 
 public class DichlofosGame extends Game implements ScreenChanger {
 
@@ -33,13 +29,13 @@ public class DichlofosGame extends Game implements ScreenChanger {
 
     @Override
     public void changeOnBattleScreen() {
-        fightScreen = new FightScreen(assets.getManager().get(FIGHT_ATLAS_FILE_NAME, TextureAtlas.class));
+        fightScreen = new FightScreen(assets.getFightTextureAtlas());
         setScreen(fightScreen);
     }
 
     @Override
     public void changeOnWorldScreen() {
-        gameScreen = new WorldScreen(this, assets.getManager().get(GTA_HERO_ATLAS_FILE_NAME, TextureAtlas.class));
+        gameScreen = new WorldScreen(this, assets.getGtaTextureAtlas());
         setScreen(gameScreen);
     }
 
--- core/src/ru/fivestarter/dichlofos/utils/Assets.java
+++ core/src/ru/fivestarter/dichlofos/utils/Assets.java
@@ -4,19 +4,23 @@ import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 public class Assets {
-    public static final String GTA_HERO_ATLAS_FILE_NAME = "atlas/gta/hero/hero.atlas";
-    public static final String FIGHT_ATLAS_FILE_NAME = "atlas/fight/fight.atlas";
+    private static final String GTA_ATLAS_FILE_NAME = "atlas/gta/gta.atlas";
+    private static final String FIGHT_ATLAS_FILE_NAME = "atlas/fight/fight.atlas";
     private final AssetManager manager;
 
     public Assets() {
         this.manager = new AssetManager();
-        manager.load(GTA_HERO_ATLAS_FILE_NAME, TextureAtlas.class);
+        manager.load(GTA_ATLAS_FILE_NAME, TextureAtlas.class);
         manager.load(FIGHT_ATLAS_FILE_NAME, TextureAtlas.class);
         manager.finishLoading();
     }
 
-    public AssetManager getManager() {
-        return manager;
+    public TextureAtlas getGtaTextureAtlas() {
+        return manager.get(GTA_ATLAS_FILE_NAME, TextureAtlas.class);
+    }
+
+    public TextureAtlas getFightTextureAtlas() {
+        return manager.get(FIGHT_ATLAS_FILE_NAME, TextureAtlas.class);
     }
 
     public void dispose() {
