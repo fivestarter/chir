679f2be46af9f9fc909ab14f3e17fc9bbcb9a377
fivestarter <fivestarter@gmail.com>
2024-09-14 00:04:41 +0300

    комбинация добавлена

--- core/src/ru/fivestarter/dichlofos/game/fight/control/FightController.java
+++ core/src/ru/fivestarter/dichlofos/game/fight/control/FightController.java
@@ -4,9 +4,13 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import org.apache.commons.lang3.tuple.MutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import ru.fivestarter.dichlofos.game.fight.model.kick.LeftHighKick;
 import ru.fivestarter.dichlofos.game.fight.model.kick.RightHighKick;
 import ru.fivestarter.dichlofos.utils.Assets;
+
+import java.util.*;
 
 public class FightController {
 
@@ -17,6 +21,8 @@ public class FightController {
     private final KickController leftMiddleKickController;
     private final KickController leftLowKickController;
 
+    private final List<Pair<KickController, Boolean>> combinationList;
+
     public FightController(Assets assets) {
         this.rightHighKickController = new KickController(new RightHighKick(assets, new Rectangle(535, 380, 34, 75)));
         this.rightMiddleKickController = new KickController(new RightHighKick(assets, new Rectangle(535, 330, 34, 75)));
@@ -24,6 +30,10 @@ public class FightController {
         this.leftHighKickController = new KickController(new LeftHighKick(assets, new Rectangle(545, 330, 34, 75)));
         this.leftMiddleKickController = new KickController(new LeftHighKick(assets, new Rectangle(545, 270, 34, 75)));
         this.leftLowKickController = new KickController(new LeftHighKick(assets, new Rectangle(545, 200, 34, 75)));
+
+        combinationList = List.of(
+                MutablePair.of(leftMiddleKickController, false),
+                MutablePair.of(rightHighKickController, false));
     }
 
     public void draw(float delta, SpriteBatch batch) {
@@ -55,5 +65,37 @@ public class FightController {
         if (Gdx.input.isKeyPressed(Input.Keys.PERIOD) && !leftLowKickController.isVisible()) {
             leftLowKickController.kick();
         }
+        if (Gdx.input.isKeyPressed(Input.Keys.T) && !isCombinationVisible() && !isCombinationReady()) {
+            combinationList.forEach(kickControllerBooleanPair -> kickControllerBooleanPair.setValue(true));
+        }
+
+        if (isCombinationReady()) {
+            handleCombination();
+        }
+    }
+
+    private void handleCombination() {
+        ListIterator<Pair<KickController, Boolean>> combinationIterator = combinationList.listIterator();
+        while (combinationIterator.hasNext()) {
+            Pair<KickController, Boolean> combination = combinationIterator.next();
+            if (combination.getValue()) {
+                if (combinationIterator.hasPrevious()) {
+                    combinationIterator.previous();
+                }
+                if (!combinationIterator.hasPrevious() || !combinationIterator.previous().getKey().isVisible()) {
+                    combination.getKey().kick();
+                    combination.setValue(false);
+                }
+                break;
+            }
+        }
+    }
+
+    private boolean isCombinationVisible() {
+        return combinationList.stream().anyMatch(combination -> combination.getKey().isVisible());
+    }
+
+    private boolean isCombinationReady() {
+        return combinationList.stream().anyMatch(Pair::getValue);
     }
 }
