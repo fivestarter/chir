56e4716b2eeb77327645510e89555b9803216189
fivestarter <fivestarter@gmail.com>
2024-09-13 12:26:59 +0300

    левый хайкик добавлен

--- core/src/ru/fivestarter/dichlofos/game/fight/control/FightController.java
+++ core/src/ru/fivestarter/dichlofos/game/fight/control/FightController.java
@@ -4,26 +4,32 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import ru.fivestarter.dichlofos.game.fight.model.kick.LeftHighKick;
 import ru.fivestarter.dichlofos.game.fight.model.kick.RightHighKick;
 import ru.fivestarter.dichlofos.utils.Assets;
 
 public class FightController {
 
-    private static final Rectangle KICK_FRAME = new Rectangle(520, 380, 34, 75);
     private final RightHighKick kick;
+    private final LeftHighKick leftHighKick;
 
     public FightController(Assets assets) {
-        this.kick = new RightHighKick(assets, KICK_FRAME);
+        this.kick = new RightHighKick(assets, new Rectangle(520, 380, 34, 75));
+        leftHighKick = new LeftHighKick(assets, new Rectangle(530, 330, 34, 75));
     }
 
     public void draw(float delta, SpriteBatch batch) {
         kick.draw(delta, batch);
+        leftHighKick.draw(delta, batch);
         handle();
     }
 
     private void handle() {
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT_BRACKET) && !kick.isVisible()) {
             kick.start();
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.P) && !leftHighKick.isVisible()) {
+            leftHighKick.start();
         }
     }
 }
---
+++ core/src/ru/fivestarter/dichlofos/game/fight/model/kick/LeftHighKick.java
@@ -0,0 +1,52 @@
+package ru.fivestarter.dichlofos.game.fight.model.kick;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Rectangle;
+import ru.fivestarter.dichlofos.game.common.Controller;
+import ru.fivestarter.dichlofos.game.fight.model.FightSprite;
+import ru.fivestarter.dichlofos.utils.Assets;
+
+public class LeftHighKick extends FightSprite implements Controller {
+
+    public static final int INIT_SCALE = 2;
+    private float scale = INIT_SCALE;
+    private float ttl = 0;
+
+    public LeftHighKick(Assets assets, Rectangle rectangle) {
+        super(assets.findFootPrint(), rectangle);
+        rotate(-40);
+        setFlip(true, false);
+        scale(scale);
+    }
+
+    @Override
+    public void draw(float delta, Batch batch) {
+        if (isVisible()) {
+            draw(batch);
+            handle(delta);
+        }
+    }
+
+    private void handle(float delta) {
+        if (scale > 1) {
+            scale -= 1.5f * delta;
+        }
+        setScale(scale);
+        ttl -= delta;
+    }
+
+    @Override
+    public Sprite getModel() {
+        return this;
+    }
+
+    public void start() {
+        scale = INIT_SCALE;
+        ttl = 1;
+    }
+
+    public boolean isVisible() {
+        return ttl > 0;
+    }
+}
